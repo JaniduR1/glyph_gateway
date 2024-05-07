24620985ccb88b8e26627e94f61bc7f98c1b5182
Janidu01 <jr798@canterbury.ac.uk>
2024-05-08 00:58:57 +0100

    Image uploader now more visible, added functionality to present the gardiner value below the image

--- lib/api_service/uploadImage.dart
+++ lib/api_service/uploadImage.dart
@@ -1,28 +1,49 @@
-// ignore: file_names
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<void> uploadImage(File imageFile) async {
-  var request = http.MultipartRequest(
-    'POST',
-    Uri.parse('http://10.0.2.2:8000/glyphServer/upload_image/'),
-  );
-  request.files.add(
-    await http.MultipartFile.fromPath(
-      'image',
-      imageFile.path,
-    ),
-  );
+Future<void> uploadImage(BuildContext context, File imageFile) async {
+  var uri = Uri.parse('http://10.0.2.2:5001/api/predict/');
+  var request = http.MultipartRequest('POST', uri);
+  var multipartFile =
+      await http.MultipartFile.fromPath('image', imageFile.path);
+  request.files.add(multipartFile);
 
   try {
     var response = await request.send();
     if (response.statusCode == 200) {
-      print('Image uploaded successfully!');
+      var responseBody = await response.stream.bytesToString();
+      var parsedResponse = jsonDecode(responseBody);
+      print('Predicted class index  ss: ${parsedResponse}');
+
+      if (parsedResponse is Map && parsedResponse.containsKey("message")) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Prediction Result'),
+              content:
+                  Text('Predicted class index  : ${parsedResponse["message"]}'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        print('Invalid result after uploading image.');
+      }
     } else {
       print('Failed to upload image: ${response.statusCode}');
     }
   } catch (e) {
-    print('Error uploading image: $e');
+    print('Error uploading image : $e');
   }
 }
--- lib/scenes/ImageTest/index.dart
+++ lib/scenes/ImageTest/index.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:clientapp/Image_helper.dart';
-import 'package:clientapp/api_service/uploadImage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:http/http.dart' as http;
@@ -10,6 +9,7 @@ final imageHelper = ImageHelper();
 
 class ImagePickerCropper extends StatelessWidget {
   const ImagePickerCropper({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,21 +38,32 @@ class ProfileImage extends StatefulWidget {
 // ignore: unused_element
 class _ProfileImageState extends State<ProfileImage> {
   File? _image;
+  String? _responseBody;
+  bool _loading = false; // Add a loading indicator variable
+
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
     return Column(
       children: [
         Center(
-            child: FittedBox(
-                fit: BoxFit.contain,
-                child: CircleAvatar(
-                    backgroundColor: Colors.grey[300],
-                    radius: 64,
-                    foregroundImage: _image != null ? FileImage(_image!) : null,
-                    child: Text(
-                      widget.initals,
-                      style: const TextStyle(fontSize: 48),
-                    )))),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              shape: BoxShape.circle,
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(32),
+              child: _image != null
+                  ? Image.file(
+                      height: screenHeight * 0.5,
+                      _image!,
+                      fit: BoxFit.cover,
+                    )
+                  : SizedBox(),
+            ),
+          ),
+        ),
         const SizedBox(height: 16),
         TextButton(
             onPressed: () async {
@@ -65,13 +76,65 @@ class _ProfileImageState extends State<ProfileImage> {
                 if (croppedFile != null) {
                   setState(() {
                     _image = File(croppedFile.path);
+                    _loading =
+                        true; // Set loading to true when processing starts
                   });
-                  await uploadImage(_image!);
+                  //################################
+                  var uri = Uri.parse('http://10.0.2.2:5001/api/predict/');
+                  var request = http.MultipartRequest('POST', uri);
+                  var multipartFile = await http.MultipartFile.fromPath(
+                      'image', croppedFile.path);
+                  request.files.add(multipartFile);
+
+                  try {
+                    var response = await request.send();
+                    if (response.statusCode == 200) {
+                      var responseBody = await response.stream.bytesToString();
+                      print(
+                          'Predicted class index: ${responseBody} ${response.statusCode}');
+                      setState(() {
+                        _responseBody = responseBody;
+                        _loading =
+                            false; // Set loading to false when result is received
+                      });
+                      _showAlertDialog(
+                          context, 'Identified Symbol : ${responseBody}');
+                    } else {
+                      print('Failed to upload image');
+                    }
+                  } catch (e) {
+                    print('Error uploading image 01: $e');
+                  }
+
+                  //#################################################
                 }
               }
             },
-            child: const Text('Capture Image'))
+            child: _loading // Display different text based on loading state
+                ? CircularProgressIndicator() // Loading indicator
+                : const Text('Capture Image')),
+        if (_responseBody != null) Text(_responseBody!),
       ],
     );
   }
+}
+
+void _showAlertDialog(BuildContext context, String message) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('GlyphGateway'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('OK'),
+          ),
+        ],
+      );
+    },
+  );
 }
